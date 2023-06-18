b987223a747d809c8bdfa989d64e80e97768e872
margin <pcentos@163.com>
2023-06-18 22:50:10 +0800

    better check kubean

--- kubean/02-check.sh
+++ kubean/02-check.sh
@@ -4,6 +4,11 @@ echo "--------------------------------------------------------------------------
 echo "#helm status kubean -n kubean-system"
 helm status kubean -n kubean-system
 echo "-----------------------------------------------------------------------------------------------------"
-echo "#helm get all kubean -n kubean-system > yaml.txt"
-helm get all kubean -n kubean-system > yaml.txt
+#echo "#helm get all kubean -n kubean-system > yaml.txt"
+#helm get all kubean -n kubean-system > yaml.txt
+#echo "-----------------------------------------------------------------------------------------------------"
+
+
+echo "#kubectl get all -n kubean-system "
+kubectl get all -n kubean-system 
 echo "-----------------------------------------------------------------------------------------------------"
